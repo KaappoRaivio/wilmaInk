681cd48a5fada63a777f4fb50210e2cb3b8b9d74
Kaappo Raivio <kaappo.raivio@gmail.com>
2020-10-17 21:29:10 +0300

    clean up AutoFitText.js
    Took 1 hour 9 minutes

--- src/AutoFitText.js
+++ src/AutoFitText.js
@@ -1,43 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
+
+const reducer = (state, { fontSize, executing }) => {
+	return { ...state, fontSize, executing };
+};
 
 const AutoFitText = ({ initialFontSize, children, className, onFontSizeFound, uniformFontSize }) => {
-	const [isExecuting, setExcecuting] = useState(true);
-	const [target, _] = useState(React.createRef());
-	const [actual, __] = useState(React.createRef());
+	const targetContainer = useState(React.createRef());
+	const actualContainer = useState(React.createRef());
 
-	const [fontSize, setFontSize] = useState(initialFontSize);
-
-	if (uniformFontSize) {
-		if (isExecuting) {
-			setExcecuting(false);
-		}
-		if (target.current && actual.current) {
-			target.current.style.fontSize = `${uniformFontSize}px`;
-			actual.current.style.fontSize = `${uniformFontSize}px`;
-		}
-	}
+	const [{ fontSize, executing }, dispatch] = useReducer(reducer, { fontSize: initialFontSize, executing: true });
 
 	useEffect(() => {
-		const currentTarget = target.current;
-		const currentActual = actual.current;
+		const target = targetContainer.current;
+		const actual = actualContainer.current;
 
-		if (currentActual && currentTarget && isExecuting) {
-			let offset = currentTarget.offsetHeight - currentActual.offsetHeight;
+		if (actual && target && executing && !uniformFontSize) {
+			let offset = target.offsetHeight - actual.offsetHeight;
+			target.style.fontSize = `${fontSize}px`;
 			if (offset < 0) {
-				// setFontSize(Math.max(fontSize + offset / 100, 0));
-				setFontSize(Math.max(fontSize - 0.1, 0));
+				dispatch({ fontSize: Math.max(fontSize - 0.1, 0), executing: true });
 			} else {
 				console.log("Size found!");
-				setExcecuting(false);
+				dispatch({ executing: false });
 				onFontSizeFound(fontSize);
 			}
-			currentTarget.style.fontSize = `${fontSize}px`;
 		}
-	}, [actual, fontSize, target, isExecuting, onFontSizeFound]);
+	}, [actualContainer, executing, fontSize, onFontSizeFound, targetContainer, uniformFontSize]);
 
 	return (
-		<div style={{ visibility: isExecuting ? "hidden" : "visible" }} className={className} ref={target}>
-			<span ref={actual}>{children}</span>
+		<div
+			style={{
+				visibility: executing ? "hidden" : "visible",
+				fontSize: uniformFontSize ? `${uniformFontSize}px` : "",
+			}}
+			className={className}
+			ref={targetContainer}>
+			<span ref={actualContainer}>{children}</span>
 		</div>
 	);
 };
